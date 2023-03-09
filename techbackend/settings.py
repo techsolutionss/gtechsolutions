ebbd1f5f08d4371958d2d8529a22a13fe690e741
codewithaule <aulekator@gmail.com>
2023-03-09 07:19:01 +0100

    changes

--- techbackend/settings.py
+++ techbackend/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
 if 'RDS_DB_NAME' in os.environ:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.mysql',
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': os.environ['RDS_DB_NAME'],
             'USER': os.environ['RDS_USERNAME'],
             'PASSWORD': os.environ['RDS_PASSWORD'],
