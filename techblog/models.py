a9cd1bf8940ec82aaeff3cd472c34b57d34a6f24
techsolutionss <aulematins@gmail.com>
2023-03-10 00:12:13 +0100

    changes

--- techblog/models.py
+++ techblog/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class PostField(models.Model):
-    title = models.CharField(max_length=150, unique=True, verbose_name='title')
+    title = models.CharField(max_length=150, unique=True, verbose_name=_('title'))
     slug = models.SlugField(max_length=150, unique=True, allow_unicode=True)
     # author= models.ForeignKey(on_delete= verbose_name='author')
     thumbnail = models.ImageField(upload_to='thumbnails')
